7098e014a30cf61c98b8fd371fc7b05ac2007a51
Marco van de Haar <marco.van.de.haar@topicus.nl>
2024-06-03 20:24:09 +0200

    rebuild for new name

--- deapp-pitch/deploy-pages.ps1
+++ deapp-pitch/deploy-pages.ps1
@@ -1,5 +1,5 @@
 # Run the build command
-& 'ng' 'build' '--output-path' '../docs' '--base-href' 'https://marcovandehaar.github.io/deaap/'
+& 'ng' 'build' '--output-path' '../docs' '--base-href' 'https://marcovandehaar.github.io/degoorsejongens/'
 
 # Copy the contents of ../docs/browser to ../docs
 Get-ChildItem -Path '../docs/browser' | Move-Item -Destination '../docs' -Force
