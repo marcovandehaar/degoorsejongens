636da3189c2a8727f0a1e28ffc61f88319e2acc0
Marco van de Haar <marco.van.de.haar@topicus.nl>
2024-06-04 21:17:00 +0200

    navbutton disable

--- deapp-pitch/src/app/app.component.ts
+++ deapp-pitch/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { Component, ViewChildren, QueryList, ElementRef, HostListener,ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,20 +6,40 @@ import { Component, ViewChildren, QueryList, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
   @ViewChildren('page') pages!: QueryList<ElementRef>;
   currentIndex = 0;
+  isAtBottom = false;
+  isAtLastPage = false;
+
+  ngAfterViewInit() {
+    this.scrollContainer.nativeElement.addEventListener('scroll', () => this.onScrollContainerScroll());
+  }
+  
 
   navigateTo(direction: 'next' | 'prev' | 'start'): void {
     // Calculate the new index
     if (direction === 'next' && this.currentIndex < this.pages.length - 1) {
       this.currentIndex++;
+      if(this.currentIndex == this.pages.length - 1){
+        this.isAtLastPage = true;
+      }
     } else if (direction === 'prev' && this.currentIndex > 0) {
       this.currentIndex--;
+      this.isAtLastPage = false;
     }else if (direction === 'start' && this.currentIndex > 0) {
       this.currentIndex=0;
+      this.isAtLastPage = false;
     }
 
     // Scroll to the new page
     this.pages.toArray()[this.currentIndex].nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  onScrollContainerScroll() {
+    const scrollPosition = this.scrollContainer.nativeElement.scrollTop + this.scrollContainer.nativeElement.clientHeight;
+    const divHeight = this.scrollContainer.nativeElement.scrollHeight;
+    //console.log(scrollPosition, divHeight);
+    this.isAtBottom = (scrollPosition >= divHeight);
   }
 }
